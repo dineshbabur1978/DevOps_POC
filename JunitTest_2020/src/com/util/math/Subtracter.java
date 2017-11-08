de1974105720b4fd23739ca99fc40c338d510bb8
dineshbabur1978 <dineshbabur1978@gmail.com>
2017-11-08 18:04:31 +0530

    Update Subtracter.java

--- JunitTest_2020/src/com/util/math/Subtracter.java
+++ JunitTest_2020/src/com/util/math/Subtracter.java
@@ -14,7 +14,7 @@ public class Subtracter
   
   public int minuser()
   {
-    System.out.println("Inside Minuser..... ");
+    System.out.println("Inside Minuser..... 23232323232323");
     return Value1-Value2;
   }
   
