bd9d63c5295dbaa1b6ee03b2cc9620b9dc1f74e2
dinesh babu r <dineshbabur1978@gmail.com>
2017-11-08 17:17:09 +0530

    Added Math Package & Adder.java

---
+++ JunitTest_2020/src/com/util/math/Adder.java
@@ -0,0 +1,24 @@
+package com.util.math;
+
+public class Adder {
+
+	int Value1=-1;
+	int Value2=-1;
+	
+	public Adder(int Arg1, int Arg2){
+		this.Value1=Arg1;
+		this.Value2=Arg2;
+	}
+	
+	/**
+	 *  Method to Add Values
+	 *  
+	 * @return
+	 */
+	public int addValues()
+	{
+		System.out.println("Inside AddValues ");
+		return Value1+Value2;
+	}
+	
+}
