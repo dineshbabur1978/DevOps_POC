94125b2f5bdf2059498933ebdc2e24fff0657fec
dinesh babu r <dineshbabur1978@gmail.com>
2017-11-08 18:53:48 +0530

    added Test Cases

---
+++ JunitTest_2020/test/com/test/util/TestAdder.java
@@ -0,0 +1,14 @@
+package com.test.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdder {
+
+	@Test
+	public void testAddValues() {
+		fail("Not yet implemented");
+	}
+
+}
