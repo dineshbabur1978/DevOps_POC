edd4b8e3ebead821d467c50e670e7f3ebfff4651
dinesh babu r <dineshbabur1978@gmail.com>
2017-11-08 17:56:23 +0530

    Adding Subtracter from Master

---
+++ JunitTest_2020/src/com/util/math/Subtracter.java
@@ -0,0 +1,17 @@
+public class Subtracter
+{
+ int Value1=-1;
+ int Value2=-1;
+ 
+  public Subtracter(int Arg1, int Arg2)
+  {
+     this.Value1=Arg1;
+     this.Value2=Arg2;
+  }
+  
+  public int minuser()
+  {
+    System.out.println("Inside Minuser..... ");
+    return Value1-Value2;
+  }
+}
