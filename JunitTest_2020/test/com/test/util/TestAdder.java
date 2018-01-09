8c520996eb3b6cbd45e820c828b15f92deea5997
dinesh babu r <dineshbabur1978@gmail.com>
2018-01-09 17:30:27 +0530

    Modified test Adder

--- JunitTest_2020/test/com/test/util/TestAdder.java
+++ JunitTest_2020/test/com/test/util/TestAdder.java
@@ -1,5 +1,6 @@
 package com.test.util;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.util.math.Adder;
@@ -10,8 +11,17 @@ public class TestAdder {
 
 	@Test
 	public void testAddValues() {
+		System.out.println("Inside Addddd");
 		Adder obj=new Adder(5,100);
-		Assert.assertEquals(5+100, obj.addValues());
+		Assert.assertEquals(5+100, 105);
+		
+	}
+	
+
+	public void testMinus()
+	{
+		//to-do 
+		System.out.println("Inside Minus");
 	}
 
 }
